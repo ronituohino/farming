18d94c35bab62bdbf750886e2ec80eefedf4d522
Roni Tuohino <rt@ronituohino.com>
2026-03-24 12:48:52 +0200

    Start working on movement

--- scenes/player.gd
+++ scenes/player.gd
@@ -5,13 +5,17 @@ extends CharacterBody3D
 
 @export_group("Camera")
 @export_range(0.0, 1.0) var mouse_sensitivity = 0.25
-
 @export var tilt_upper_limit := PI / 3.0
 @export var tilt_lower_limit := -PI / 6.0
 
+@onready var _camera: Node3D = %Camera3D
 @onready var _camera_pivot: Node3D = %CameraPivot
 
 var _camera_input_direction := Vector2.ZERO
+
+@export_group("Movement")
+@export var move_speed := 8.0
+@export var acceleration := 20.0
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
@@ -32,3 +36,6 @@ func _physics_process(delta: float) -> void:
 	_camera_pivot.rotation.x = clamp(_camera_pivot.rotation.x, tilt_lower_limit, tilt_upper_limit)
 	_camera_pivot.rotation.y -= _camera_input_direction.x * delta
 	_camera_input_direction = Vector2.ZERO
+	
+	var raw_input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	print(_camera.global_basis.z)
